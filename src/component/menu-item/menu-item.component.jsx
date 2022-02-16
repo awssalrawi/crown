0d88436504fe4545bc176f419b79cd6745e88ecd
Awss Nafea <awss.alrawi@gmail.com>
2022-02-16 17:46:38 +0300

    add router package and navigate our page

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 import "./menu-item.styles.scss";
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => {
   return (
-    <div className={`${size ? size : ""} menu-item`}>
+    <div
+      className={`${size ? size : ""} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{
@@ -16,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     </div>
   );
 };
-export default MenuItem;
+export default withRouter(MenuItem);
